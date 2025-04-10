0dcc9353ecc769a7613837d8ed011a79d29c0a0a
AjayAjju18 <ajaypolepally18@gmail.com>
2025-04-10 08:58:08 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,30 @@
-import java.util.*;
-class main
-{
-  public static void main(String[] args)
-  {
-      System.out.println("DevOps is on thursday");
-  }
+ class Prime {
 
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        // Only check up to the square root of num
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int limit = 100;
+
+        System.out.println("Prime numbers up to " + limit + " are:");
+
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
-  
